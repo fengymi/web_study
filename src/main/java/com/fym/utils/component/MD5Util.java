4a6292e556c7eb7f0e7b19ae7aa37a255204ed9a
fengymi <943236288@qq.com>
2016-12-02 11:54:25 +0800

    区分大小写

--- src/main/java/com/fym/utils/component/MD5Util.java
+++ src/main/java/com/fym/utils/component/MD5Util.java
@@ -6,7 +6,7 @@ public class MD5Util {
     public final static String MD5(String s) {
         char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         try {
-            byte[] btInput = (s.toLowerCase()+"@.feng").getBytes();
+            byte[] btInput = (s+"@.feng").getBytes();
             // 获得MD5摘要算法的 MessageDigest 对象
             MessageDigest mdInst = MessageDigest.getInstance("MD5");
             // 使用指定的字节更新摘要
